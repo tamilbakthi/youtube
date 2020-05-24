1322596b8af31c1496c037e912e03deb6c116289
Pratheepag <pratheepa.vnr@gmail.com>
2020-05-24 09:04:25 +0530

    Modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { CategoryListingComponent } from './category-listing/category-listing.component';
 import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import {APP_BASE_HREF} from '@angular/common'; 
 
 
 @NgModule({
@@ -21,7 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
     NgbModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/youtube'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
